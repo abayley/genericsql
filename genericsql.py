c8ecf0ccff2df18a336a8e49b8d54116e378d462
Alistair Bayley <alistair@abayley.org>
2018-05-17 12:26:21 +1200

    Fix file redirection stuff in command line. Fiddling trying to get incremental subprocess output.

--- genericsql.py
+++ genericsql.py
@@ -5,6 +5,8 @@ import sublime_plugin
 import subprocess
 import functools
 import traceback
+import time
+
 
 # https://www.sublimetext.com/docs/3/api_reference.html
 
@@ -16,7 +18,7 @@ We use the build system so that we can interactively choose which command
 (i.e. which database) to run from the list of build system variants.
 
 We inherit from sublime_plugin.WindowCommand, so that the class SqlExecCommand
-automatically becomes a command: sql_exec [the run() method is invoked].
+automatically becomes the command "sql_exec" (the run() method is invoked).
 This new command then referenced in the .sublime-build file as the target e.g.
 {
     // selector only works when build system is Automatic
@@ -55,6 +57,36 @@ In each of these cases "dialect" and "cmd" are not set. Here is what happens for
 
 If you get "OSError: [Errno 8] Exec format error" then you need to add #! to your command scripts:
 https://stackoverflow.com/questions/27606653/oserror-errno-8-exec-format-error
+
+
+Setting up build commands:
+Probably best to create a script in your personal bin which runs the command
+(so you can easily reuse from terminal etc) e.g.
+
+{
+    "selector": "source.sql",
+    "target": "sql_exec",
+    "cmd" : "",
+    "variants": [
+        { "name": "Run", "cmd" : "reset" },
+        { "name": "user@database",        "dialect": "oracle",   "cmd": ["oradb1"] },
+        { "name": "user@database.host",   "dialect": "postgres", "cmd": ["pgdb1"] },
+        { "name": "user@database.host",   "dialect": "mysql",    "cmd": ["mydb1"] }
+    ]
+}
+
+where the scripts contain commands like so:
+
+[mydb1]
+#!/bin/sh
+mysql -A -t -h fqdn.server.name -P 3306 -u username -ppassword -D dbname "$*"
+
+[oradb1]  # note rlwrap sexy
+#!/bin/sh
+rlwrap sqlplus -s username/password@\"fqdn.server.name:1521/orasid\" $*
+
+[pgdb1]
+psql "host=fqdn.server.name user=username password=password dbname=dbname" $*
 """
 
 # If you want to manipulate the text in a view, the best way now (ST3) is to
@@ -66,12 +98,17 @@ class AppendTextCommand(sublime_plugin.TextCommand):
         self.view.insert(edit, self.view.size(), text)
 
 
+class ClearViewCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.erase(edit, sublime.Region(0, self.view.size()))
+
+
 class SqlExecCommand(sublime_plugin.WindowCommand):
 
     def log(self, text, panel_name="sql"):
+        return  # suppress logging
         # get_output_panel doesn't "get" the panel, it *creates* it,
         # so we should only call get_output_panel once
-        # self.window = self.get_window()
         if not hasattr(self, "output_panel"):
             self.output_panel = self.window.get_output_panel(panel_name)
         v = self.output_panel
@@ -121,6 +158,7 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         scroll_to_end = (len(view.sel()) == 1 and view.sel()[0] == sublime.Region(view.size()))
         # Normalize newlines, Sublime Text always uses a single \n separator in memory.
         line = line.replace('\r\n', '\n').replace('\r', '\n')
+        # This can be slow for large output
         view.run_command("append_text", {"text" : line})
         if scroll_to_end:
             view.show(view.size())
@@ -128,25 +166,39 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
     def kill_shell(self):
         if hasattr(self, "shell_process"):
             if self.shell_process:
+                self.log("kill_shell: call terminate on %s" % str(self.shell_process))
                 self.shell_process.terminate()
-                self.shell_process = None
+            else:
+                self.log("kill_shell: no process to kill")
 
     def shell_command(self, cmd):
-        view = self.output_view
         try:
-            popen = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+            popen = subprocess.Popen(cmd, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
             self.shell_process = popen
+            popen.poll()
+            self.log("shell_command: while")
             while popen.returncode is None:
+                popen.poll()
+                self.log("shell_command: popen.poll %s  at %s" % (popen.returncode, time.time()))
                 # popen.communicate does not return/produce output until process terminates.
                 # (might be a shell buffering thing). So instead use readline to get output so far.
-                out = popen.stdout.readline()
-                self.append_text(out)
-                popen.poll()
+                try:
+                    (out, _) = popen.communicate(timeout=0.20)
+                    self.append_text(out)
+                except subprocess.TimeoutExpired:
+                    self.log("shell_command: communicate timeout, use readlines  at %s" % time.time())
+                    # out = popen.stdout.readlines()
+                    # self.log("shell_command: readlines append  at %s" % time.time())
+                    # self.append_text("".join(out))
             # get remaining output
-            popen.wait()
-            (out, _) = popen.communicate()
-            self.append_text(out)
-            self.append_text("[Return code: %s]\n" % popen.returncode)
+            try:
+                self.log("shell_command: returncode not None, get remaining output with popen.communicate")
+                (out, _) = popen.communicate()
+                self.log("shell_command: append output")
+                self.append_text(out)
+            except ValueError:  # output handle closed causes this
+                pass
+            self.append_text("\n[Return code: %s]\n" % popen.returncode)
             popen.stdout.close()
             self.shell_process = None
 
@@ -156,6 +208,9 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         except OSError as e:
             if e.errno == 2:
                 self.append_text("Command not found: %s" % str(cmd))
+            elif e.errno == 8:
+                self.append_text(str(e))
+                self.append_text("\n(if you are executing a script, ensure the first line has a she-bang #!)\n")
             else:
                 self.append_text(str(e))
 
@@ -185,6 +240,7 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         if view is None:
             view = self.window.new_file()
 
+        view.run_command("clear_view")
         self.output_view = view
         self.window.focus_view(view)
         view.show(view.size())  # scroll to end
@@ -199,15 +255,16 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         view.settings().set("result_base_dir", working_dir)
 
         if file_name:
-            # requires the self.output_view is already set
+            # requires that self.output_view is already set
             self.append_text("Filename: " + file_name + "\n")
         if file_regex:
             view.settings().set("result_file_regex", file_regex)
         if line_regex:
             view.settings().set("result_line_regex", line_regex)
 
-        # Use main_thread to run async - in another thread
-        main_thread(self.shell_command, cmd)
+        # Use set_timeout_async to run async - in another thread
+        # https://forum.sublimetext.com/t/execute-external-program-in-background-non-blocking-thread/11122/6
+        sublime.set_timeout_async(functools.partial(self.shell_command, cmd), 0)
 
     def run_file(self, view, cmd, file_name, **kwargs):
         """
@@ -215,21 +272,24 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         """
         dialect = kwargs.pop("dialect")
         # Add filename to command args. Some clients will work with stdin redirection,
-        # other require a specific file argument.
-        # oracle and mysql are OK with stdin redirect
-        append = "<" + file_name
-        # if dialect == "mysql":
-        #     append = "-e source " + file_name
+        # others require a specific file argument.
+        # oracle and mysql are OK with stdin redirect.
+        # TODO test and rationalise this code (to add input filename)
+        append = ["<" + file_name]
+        if dialect == "mysql":
+            append = ["-e source " + file_name]
         if dialect == "postgres":
-            append = "-f" + file_name
-        cmd.append(append)
+            append = ["-f" + file_name]
+        if dialect == "oracle":
+            append = [file_name]
+        cmd += append
         # Setting/passing these regexes allow ST to parse error messages and
         # take you to the source file if you double-click on the,.
         file_regex = "^Filename: (.+)$"
         line_regex = "^\\(.+?/([0-9]+):([0-9]+)\\) [0-9]+:[0-9]+ (.+)$"
         syntax = "Packages/SQL/SQL.sublime-syntax"
 
-        # long-running command, useful for testing
+        # long-running command, useful for performance testing
         # cmd = ["locate", "a"]
         self.run_command(cmd, syntax=syntax, file_name=view.file_name(), file_regex=file_regex, line_regex=line_regex)
 
@@ -261,6 +321,7 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
         """
         (handle, temp_file_name) = tempfile.mkstemp(suffix=".sql", text=True)
         if kwargs.get("dialect") == "oracle":
+            # set autotrace on
             os_write(handle, "explain plan for ")
             self.write_selection_to_handle(view, handle)
             os_write(handle, "\nset heading off")
@@ -288,11 +349,11 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
             sqlscope: file (f7), statement (f8)
             action: reset (shift+f7/f8), explain (ctrl+shift+f8), execute (f7/f8)
         """
-        # self.reset_log()
-        # self.log("---------- run start")
-        # for (k, v) in kwargs.items():
-        #     self.log(str(k) + " : " + str(v))
-        # self.log("----------")
+        self.reset_log()
+        self.log("---------- run start")
+        for (k, v) in kwargs.items():
+            self.log(str(k) + " : " + str(v))
+        self.log("----------")
 
         if kwargs.get("kill", False):
             self.kill_shell()
@@ -359,8 +420,3 @@ class SqlExecCommand(sublime_plugin.WindowCommand):
 
 def os_write(handle, s):
     os.write(handle, bytes(s, "UTF-8"))
-
-
-# https://forum.sublimetext.com/t/execute-external-program-in-background-non-blocking-thread/11122/6
-def main_thread(callback, *args, **kwargs):
-    sublime.set_timeout_async(functools.partial(callback, *args, **kwargs), 0)
